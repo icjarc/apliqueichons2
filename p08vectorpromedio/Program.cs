e8c19d566b8c124974b6c6a56a0b43436c15d1cd
icjarc <ic.jarc@gmail.com>
2020-09-20 22:05:43 -0500

    nvo

---
+++ p08vectorpromedio/Program.cs
@@ -0,0 +1,38 @@
+﻿// Programa que calcula el promedio de un vector con 50 elementos además ydetermina cuántos son mayores al promedio
+// Felix Fernando Tonche Valadez
+
+using System;
+
+namespace p08vectorpromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] vector = {10,20,30,40,50,60,70,80,90,100,
+                             10,20,30,40,50,60,70,80,90,100,
+                             10,20,30,40,50,60,70,80,90,100,
+                             10,20,30,40,50,60,70,80,90,100,
+                             10,20,30,40,50,60,70,80,90,100};
+            int suma=0, nmp=0;
+            float prom;      // promedio
+
+            for(int i=0; i<vector.Length; i++ ){
+                Console.Write($"{vector[i]} ");
+                suma+=vector[i];
+            }
+            prom = suma / vector.Length;
+            Console.WriteLine($"\nEl promedio es: {prom} \n");
+
+            
+            foreach(int v in vector){
+                if(v>prom){
+                    Console.Write($"{v} ");
+                    nmp++;
+                } 
+            }
+            Console.WriteLine($"\nElementos mayores al promedio: {nmp} \n");
+            
+        }
+    }   
+}
