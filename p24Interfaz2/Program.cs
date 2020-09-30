46e865724e9043c46d98f583e54655bf3d70913e
icjarc <ic.jarc@gmail.com>
2020-09-30 02:37:39 -0500

    interfazv1

---
+++ p24Interfaz2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace p24Interfaz2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
