47d9047a9cec41ecdda64b1b2e52c192a55531c2
icjarc <ic.jarc@gmail.com>
2020-10-06 02:05:17 -0500

    del 1

---
+++ p25delegados/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace p25_Delegados1
+{
+    //Declaramos el delegado
+    public delegate void MiDelegado(string msj);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiDelegado d;   //Se instancia delagado
+            
+            Console.Clear();
+
+            //Delegado invoca al método Mensaje1 de la clase Mensajes
+            d = Mensajes.Mensaje1;
+            d("Juan");
+
+            //Delegado invoca al método Mensaje2 de la clase Mensajes
+            d = Mensajes.Mensaje2;
+            d("Jose");
+
+            //Delegado invoca a una expresión lambada
+            d = (string msj) => Console.WriteLine($"{msj} - paga todo, que no pare la fiesta");
+            d("Arthur");
+
+            Console.WriteLine();
+        }
+    }
+
+    public class Mensajes{
+        public static void Mensaje1(string msj) => Console.WriteLine($"{msj} - lleva el pastel");
+        public static void Mensaje2(string msj) => Console.WriteLine($"{msj} - fue el culpable, se cancela la fiesta");
+    }
+}
+//RenteriaCabreraJulioArturo
