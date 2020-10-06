81ac5edca9617d195149bf6eb8c90f290de892d7
icjarc <ic.jarc@gmail.com>
2020-10-06 02:15:33 -0500

    Del 3

---
+++ p27delegados3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace p27_Delegados3
+{
+    //Declarar el delegado
+    public delegate int MiDelegado(int a, int b);
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MiDelegado d1 = A.MetodoA;
+            MiDelegado d2 = B.MetodoB;
+
+            //Invocar delegados de forma separada
+            Console.WriteLine($"La suma es: {d1(10,20)}");
+            Console.WriteLine($"La multiplicación es: {d2(10,20)}");
+            Console.WriteLine();
+
+            //Delegado multicast que une los 2 métodos
+            MiDelegado d = d1 + d2;
+            Console.WriteLine($"El resultado es: {d(5,2)}");    //Regresa el resultado del último método invocado
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+    }
+
+    public class A{
+        public static int MetodoA(int a, int b) => a + b;
+    }
+
+    public class B{
+        public static int MetodoB(int a, int b) => a * b;
+    }
+}
